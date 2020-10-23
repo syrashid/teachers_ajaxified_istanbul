34cc323129276d4393c3d7c30260f72a2f6f4a8f
Sy Rashid <sy.rashid.11@gmail.com>
2020-10-23 09:23:37 +0300

    FEAT: Set up index and show page for teachers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'teachers/index'
+  get 'teachers/show'
   root to: 'pages#home'
   resources :teachers, only: [ :index, :show ] do
     resources :reviews, only: :create
